7518d4c5d7089577af5867a85c4605651004a898
chancity <hawbakerchancey@gmail.com>
2019-03-22 01:25:01 -0700

    formatting change

--- kin-leaderboard-api/Services/OperationsService.cs
+++ kin-leaderboard-api/Services/OperationsService.cs
@@ -9,9 +9,8 @@ namespace kin_leaderboard_api.Services
     public class OperationsService : AbstractService<AppOperationDto, Operation, long>
     {
 
-        public OperationsService(ILoggerFactory loggerFactory, ApplicationContext context, IMapper mapper) : base(loggerFactory.CreateLogger<OperationsService>(), context, mapper)
-        {
-        }
+        public OperationsService(ILoggerFactory loggerFactory, ApplicationContext context, IMapper mapper) 
+            : base(loggerFactory.CreateLogger<OperationsService>(), context, mapper) {}
     }
 
 }
